a94030a04056045aaee9300fc0564057d19c72b0
davutcagri <71286320+davutcagri@users.noreply.github.com>
2023-07-05 22:30:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 10; i++) {
+            for(int y = 0; y < i; y++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+
+    }
+}
